6b77ed6bc8823f5f78c4cf74da8239d0ab3b8ee0
JAZ Team <98105605+mcaJaz@users.noreply.github.com>
2022-06-08 01:15:07 +0700

    Update mcaddos.py

--- mcaddos.py
+++ mcaddos.py
@@ -61,12 +61,12 @@ def logo():
 \t ************ Moslem Cyber Army **********
 \t ~~~~~~~~~~~~~ A    S    C   A ~~~~~~~~~~~
 
-\t =============[%s MCA DDos V1.0 %s]============  
-\t              Version : python 1.x.x
-\t                 UNLIMITED TIME!
-\t%s   ReproClone By JazCyberTeam|21/02/2022 %s
-\t% ==========================================%  
-    ''')
+\t =========[%s MCA DDos V1.0 %s]===========  
+\t          Version : python 1.x.x
+\t              UNLIMITED TIME!
+\t   ReproClone By JazCyberTeam|21/02/2022 
+\t =========================================  
+''')
     (Y,G,W,R,G)
 
     try:
