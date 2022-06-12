ef8c90bfc0e65db8626b0ff3ea089d1360720e9f
JAZ Team <98105605+mcaJaz@users.noreply.github.com>
2022-06-13 01:36:24 +0700

    Update mcaddos.py

--- mcaddos.py
+++ mcaddos.py
@@ -65,7 +65,7 @@ def logo():
 \t   ReproClone By JazCyberTeam|21/02/2022    
 \t =========================================  
 ''')
-        os.system("figlet -f  slant  MCA2022")
+    os.system("figlet -f  slant       MCA2022")
 
     try:
         print("\n[*] Target : "+W+str(url_main)+ ":" +str(port)+G)
